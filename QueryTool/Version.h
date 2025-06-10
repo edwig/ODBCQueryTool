41e261c6e37c2b3b681fd9927c8bb1d6d070b1c7
Edwig <edwig.huisman@hetnet.nl>
2025-06-10 13:36:13 +0200

    Version 3.4.5

--- QueryTool/Version.h
+++ QueryTool/Version.h
@@ -24,16 +24,16 @@
 #pragma once
 
 // General text version
-#define ODBCQUERYTOOL_VERSION   _T("3.4.4")
-#define ODBCQUERYTOOL_BUILD     _T("418")
-#define ODBCQUERYTOOL_COPYRIGHT _T("Copyright (c) Edwig Huisman 2004-2024")
+#define ODBCQUERYTOOL_VERSION   _T("3.4.5")
+#define ODBCQUERYTOOL_BUILD     _T("424")
+#define ODBCQUERYTOOL_COPYRIGHT _T("Copyright (c) Edwig Huisman 2004-2025")
 
 // For the resource files
 #define QT_RES_VERSION 3
 #define QT_RES_MAJOR   4
-#define QT_RES_MINOR   4
-#define QT_RES_BUILD   418
-#define QT_RES_FILE    _T("3,4,4,418")
+#define QT_RES_MINOR   5
+#define QT_RES_BUILD   424
+#define QT_RES_FILE    _T("3,4,5,424")
 
 // For the registry and the theme framework
 #define PRODUCT_REGISTRY _T("EDO\\ODBCQueryTool")
